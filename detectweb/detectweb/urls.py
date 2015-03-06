3caf717918291fa4d9bf64efcdce4fde7017130f
Yan Zhao <zhaoyan1117@gmail.com>
2015-03-06 15:20:14 -0800

    started urls and views~~

--- detectweb/detectweb/urls.py
+++ detectweb/detectweb/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'detectweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.image_drop_box),
+    url(r'^upload/$', views.upload_images),
+    url(r'^reports/$', views.index_reports),
+    url(r'^reports/(?P<report_id>\d+)/$', views.show_reports),
 )
---
+++ detectweb/detectweb/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods, require_POST, require_GET
+
+import datetime
+
+@require_GET
+def image_drop_box(request):
+    html = "<html><body>dropbox</body></html>"
+    return HttpResponse(html)
+
+@require_POST
+def upload_images(request):
+    html = "<html><body>upload</body></html>"
+    return HttpResponse(html)
+
+@require_GET
+def index_reports(request):
+    html = "<html><body>index</body></html>"
+    return HttpResponse(html)
+
+@require_GET
+def show_reports(request, **params):
+    html = "<html><body>show %s </body></html>" % params['report_id']
+    return HttpResponse(html)
