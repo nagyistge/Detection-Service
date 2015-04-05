4919424b5da7daad8f821c51cffe99ab27e796cb
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-04 19:08:49 -0700

    tempalte hello world

--- detectweb/detectweb/reports/views.py
+++ detectweb/detectweb/reports/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
 from django.views.decorators.http import require_http_methods, require_POST, require_GET
 
-import datetime
 
 @require_GET
 def image_drop_box(request):
-    html = "<html><body>dropbox</body></html>"
-    return HttpResponse(html)
+    context = {'name': 'blah'}
+    return render(request, 'reports/image_drop_box.html', context)
 
 @require_POST
 def upload_images(request):
--- detectweb/detectweb/urls.py
+++ detectweb/detectweb/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', reports_views.image_drop_box),
-    url(r'^upload/$', reports_views.upload_images),
-    url(r'^reports/$', reports_views.index_reports),
-    url(r'^reports/(?P<report_id>\d+)/$', reports_views.show_reports),
+    url(r'^$', reports_views.image_drop_box, name='image_drop_box'),
+    url(r'^upload/$', reports_views.upload_images, name='upload_images'),
+    url(r'^reports/$', reports_views.index_reports, name='index_reports'),
+    url(r'^reports/(?P<report_id>\d+)/$', reports_views.show_reports, name='show_reports'),
 )
