9075c2ae9c4d660447f8badb683fa79371d0b5ae
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-08 19:58:42 -0700

    added ela

--- detectweb/detectweb/image_analysis.py
+++ detectweb/detectweb/image_analysis.py
@@ -2,16 +2,16 @@ from __future__ import absolute_import
 
 import numpy as np
 
+from imforensics.util import numpy2matlb
+from imforensics import ela
+
 def analyze(report):
     img_file = r.file_path
 
-    feature_vec = []
-    feature_vec.append(do_copy_move(img_file))
-    feature_vec.append(do_ela(img_file))
-    feature_vec.append(do_metadata(img_file))
-    feature_vec.append(do_splicing(img_file))
-    feature_vec = np.array(feature_vec)
-    feature = do_aggregator(feature_vec)
+    cm_result = do_copy_move(img_file)
+    ela_result = do_ela(img_file)
+    ho_result = do_higher_order(img_file)
+    feature = do_aggregator(cm_result, ela_result, ho_result)
     classifier_result = do_classifier(feature)
 
     report.score = classifier_result
@@ -22,15 +22,13 @@ def do_copy_move(img_file):
     pass
 
 def do_ela(img_file):
+    e = ela.ELA(img_file)
+    return numpy2matlb(e.ela_image_scaled.astype(np.double))
+
+def do_higher_order(img_file):
     pass
 
-def do_metadata(img_file):
-    pass
-
-def do_splicing(img_file):
-    pass
-
-def do_aggregator(feature_vec):
+def do_aggregator(cm_result, ela_result, ho_result):
     pass
 
 def do_classifier(feature):
