e65f3112a0fdf033a9e71060f9a38ef2f4c422dd
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-08 18:43:39 -0700

    update image analysis

--- detectweb/detectweb/image_analysis.py
+++ detectweb/detectweb/image_analysis.py
@@ -6,12 +6,13 @@ def analyze(report):
     img_file = r.file_path
 
     feature_vec = []
-    feature_vec.extend(do_copy_move(img_file))
-    feature_vec.extend(do_ela(img_file))
-    feature_vec.extend(do_metadata(img_file))
-    feature_vec.extend(do_splicing(img_file))
+    feature_vec.append(do_copy_move(img_file))
+    feature_vec.append(do_ela(img_file))
+    feature_vec.append(do_metadata(img_file))
+    feature_vec.append(do_splicing(img_file))
     feature_vec = np.array(feature_vec)
-    classifier_result = do_classifier(feature_vec)
+    feature = do_aggregator(feature_vec)
+    classifier_result = do_classifier(feature)
 
     report.score = classifier_result
     report.is_finished = True
@@ -29,5 +30,8 @@ def do_metadata(img_file):
 def do_splicing(img_file):
     pass
 
-def do_classifier(feature_vec):
+def do_aggregator(feature_vec):
+    pass
+
+def do_classifier(feature):
     pass
