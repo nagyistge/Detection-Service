a3c58b3d4bf7c0f9b3f276a37cf3def47e994f83
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-08 20:54:45 -0700

    copy move done

---
+++ detectweb/detectweb/image_analyzer.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+import numpy as np
+
+import matlab.engine
+
+from imforensics.util import numpy2matlb
+from imforensics import ela
+from imforensics import copymove
+
+class ImageAnalyzer(object):
+
+    def __init__(self):
+        self.eng = matlab.engine.start_matlab()
+
+    def analyze(self, report):
+        self.report = report
+        img_file = report.file_path
+
+        cm_result = self._do_copy_move(img_file)
+        ela_result = self._do_ela(img_file)
+        ho_result = self._do_higher_order(img_file)
+        feature = self._do_aggregator(cm_result, ela_result, ho_result)
+        classifier_result = self._do_classifier(feature)
+
+        self.report.score = classifier_result
+        self.report.is_finished = True
+        self.report.save()
+        self.eng.quit()
+
+    def _do_copy_move(self, img_file):
+        c = copymove.CopyMoveDetector(self.eng)
+        ransac_img, ransac_matches = c.detect(img_file)
+        # TODO: save points to db.
+        return ransac_img
+
+    def _do_ela(self, img_file):
+        e = ela.ELA(img_file)
+        return numpy2matlb(e.ela_image_scaled.astype(np.double))
+
+    def _do_higher_order(self, img_file):
+        pass
+
+    def _do_aggregator(self, cm_result, ela_result, ho_result):
+        pass
+
+    def _do_classifier(self, feature):
+        pass
--- detectweb/detectweb/tasks.py
+++ detectweb/detectweb/tasks.py
@@ -3,10 +3,15 @@ from __future__ import absolute_import
 from detectweb.celery import app
 from celery.utils.log import get_task_logger
 
-from image_analysis import analyze
+from image_analyzer import ImageAnalyzer
+import time
 
 @app.task
 def generate_report(report):
     logger = get_task_logger('Report Generator')
     logger.info('Running analysis for report {0}'.format(report.id))
-    analyze(report)
+    start = time.time()
+    analyzer = ImageAnalyzer()
+    analyzer.analyze(report)
+    duration = int(time.time() - start)
+    logger.info('Report {0} is analyzed; TIME: {1}'.format(report.id, duration))
